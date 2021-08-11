54009daa368c95e2883d3ffd75c1a9afed5c1ed0
rmtar <picpoint@yandex.ru>
2021-08-11 16:54:04 +0300

    work with registration

--- app/Http/Controllers/Admin/MainController.php
+++ app/Http/Controllers/Admin/MainController.php
@@ -13,10 +13,6 @@ class MainController extends Controller
     public function index() {
         $title = "Admin page";
 
-        $tag = new Tag();
-        $tag->title = 'корова му му';
-        $tag->save();
-
         return view('admin.index', compact('title'));
     }
 
